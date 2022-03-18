3af1730892235b690edfe0892f41f74104b043e2
manfredhq <hugo.quitton@gmail.com>
2022-03-18 13:56:36 +0100

    Added, text component changeable

--- Engine/Engine/TextComponent.h
+++ Engine/Engine/TextComponent.h
@@ -1,10 +1,30 @@
 #pragma once
+
+
 class MeshComponent;
 class TextComponent :
     public MeshComponent
 {
+public:
+    LPCWSTR _txt;
+    D3DXCOLOR _color;
+    D3DXVECTOR2 _topLeftCorner;
+    D3DXVECTOR2 _bottomRightCorner;
+
+    ID3DXFont* _font;
+
+private:
+    RECT rect;
 
 public:
+    TextComponent(GameObject* GO);
+    virtual ~TextComponent();
+
     virtual void Draw() override;
+
+    virtual void InitComponent() override;
+
+    void InitFont(ID3DXFont* font);
+    void InitText(LPCWSTR txt, D3DXCOLOR color, D3DXVECTOR2 topLeftCorner, D3DXVECTOR2 bottomRightCorner);
 };
 
