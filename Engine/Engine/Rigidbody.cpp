822a0d88ba42418c7c6b555adc97c5af0556b0c6
Licavin <74245800+Licavin@users.noreply.github.com>
2022-03-23 16:32:56 +0100

    Rigidbody

--- Engine/Engine/Rigidbody.cpp
+++ Engine/Engine/Rigidbody.cpp
@@ -1,4 +1,75 @@
 #include "pch.h"
+#include "framework.h"
 #include "Rigidbody.h"
+
+void Rigidbody::Init()
+{
+	transform = _gameObject->_transform;
+	timer = _engine->GetTimer();
+}
+
+void Rigidbody::Update()
+{
+	velocity += acc * timer->deltaTime;
+	pos += velocity * timer->deltaTime;
+	transform->ChangePosition(pos);
+
+}
+
+void Rigidbody::SetGravity(bool b)
+{
+	if (gravityEnabled !=b)
+	{
+		if (b==true)
+		{
+			acc.y += g;
+		}
+		else if (b==false)
+		{
+			acc.y -= g;
+		}
+		
+	}
+}
+
+void Rigidbody::AddVelocity(D3DXVECTOR3 vvect)
+{
+	velocity += vvect;
+}
+
+void Rigidbody::AddVelocityX(float vx)
+{
+	velocity.x += vx;
+}
+
+void Rigidbody::AddVelocityY(float vy)
+{
+	velocity.y += vy;
+}
+
+void Rigidbody::AddVelocityZ(float vz)
+{
+	velocity.z += vz;
+}
+
+void Rigidbody::AddForce(D3DXVECTOR3 avect)
+{
+	acc += avect;
+}
+
+void Rigidbody::AddForceX(float ax)
+{
+	acc.x += ax;
+}
+
+void Rigidbody::AddForceY(float ay)
+{
+	acc.y += ay;
+}
+
+void Rigidbody::AddForceZ(float az)
+{
+	acc.z += az;
+}
 
 
--- Engine/Engine/Rigidbody.h
+++ Engine/Engine/Rigidbody.h
@@ -5,8 +5,29 @@ class Rigidbody :
 {
 public:
     MeshComponent* mesh;
+   
+    void Init();
+    virtual void Update() override;
+    void SetGravity(bool b);
+    void AddVelocity(D3DXVECTOR3 vvect);
+    void AddVelocityX(float vx );
+    void AddVelocityY(float vy);
+    void AddVelocityZ(float vz);
+    void AddForce(D3DXVECTOR3 avect);
+    void AddForceX(float ax);
+    void AddForceY(float ay);
+    void AddForceZ(float ax);
 
 
+private:
+    bool IsInit;
+    bool gravityEnabled;
+    D3DXVECTOR3 pos;
+    D3DXVECTOR3 velocity;
+    D3DXVECTOR3 acc;
+    float g = 9.81f;
 
+    Transform* transform;
+    STimer* timer;
 };
 
